d3dc8e5af50c20199078b284b01159421a0b1564
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-02-08 16:36:26 +0700

    feat: initial endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,11 @@ const route = require('express').Router();
 
 route.use('/vehicles', require('./vehicles'));
 
+route.get('/', (req, res)=> {
+    return res.json({
+        success: true,
+        message: 'Backend is running well!'
+    })
+})
+
 module.exports = route;
