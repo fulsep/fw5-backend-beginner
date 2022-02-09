82fbf287e6ddaeeea938c34dd289b943a52c9f5d
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-02-09 15:14:31 +0700

    feat: attach controller to router

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,10 +1,11 @@
 const vehicles = require('express').Router();
 
-const {getVehicles, getVehicle, createVehicle} = require('../controllers/vehicles');
+const {getVehicles, getVehicle, createVehicle, updateVehicle} = require('../controllers/vehicles');
 
 vehicles.get('/', getVehicles);
 vehicles.post('/', createVehicle);
 vehicles.get('/:id', getVehicle);
+vehicles.patch('/:id', updateVehicle);
 
 
 module.exports = vehicles;
