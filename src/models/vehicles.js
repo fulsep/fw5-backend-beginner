8bfc69366eb8b87bc0ead9690cc82082e20590a9
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-02-09 15:10:26 +0700

    feat: create some async model

--- src/models/vehicles.js
+++ src/models/vehicles.js
@@ -8,11 +8,18 @@ exports.getVehicles = (cb) => {
 };
 
 exports.getVehicle = (id, cb) => {
-    db.query('SELECT * FROM vehicles WHERE id=?', [id], (err, res) => {
+    db.query('SELECT id, name, image, price, (price * 20/100) as minPrepayment, qty, category_id, createdAt, updatedAt FROM vehicles WHERE id=?', [id], (err, res) => {
         if (err) throw err;
         cb(res);
     });
 };
+
+exports.getVehicleAsync = (id) => new Promise((resolve, reject)=> {
+  db.query('SELECT * FROM vehicles WHERE id=?', [id], (err, res) => {
+      if (err) reject(err);
+      resolve(res);
+  });
+});
 
 exports.createVehicle = (data, cb) => {
     db.query('INSERT INTO `vehicles` (name, image, price, qty, category_id) VALUES (?, ?, ?, ?, ?)', [data.name, data.image, data.price, data.qty, data.category_id], (err, res)=> {
@@ -20,3 +27,17 @@ exports.createVehicle = (data, cb) => {
         cb(res)
     })
 }
+
+exports.updateVehicle = (data, id, cb) => {
+  db.query('UPDATE `vehicles` SET ? WHERE id=?', [data, id], (err, res)=> {
+      if(err) throw err;
+      cb(res)
+  })
+}
+
+exports.updateVehicleAsync = (data, id) => new Promise((resolve, reject)=> {
+  db.query('UPDATE `vehicles` SET ? WHERE id=?', [data, id], (err, res)=> {
+      if(err) reject(err);
+      resolve(res) // Object => affectedRows
+  })
+})
