220eacb5396dd7386eb3683b1ee77681e2e96c5e
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-02-09 15:14:17 +0700

    feat: create update controller

--- src/controllers/vehicles.js
+++ src/controllers/vehicles.js
@@ -1,7 +1,8 @@
 const vehicleModel = require('../models/vehicles');
 const {APP_URL} = process.env
 const upload = require('../helpers/upload').single('image');
-const fs = require('fs')
+const fs = require('fs');
+const response = require('../helpers/response');
 
 const getVehicles = (req, res) => {
     vehicleModel.getVehicles(results => {
@@ -25,19 +26,20 @@ const getVehicle = (req, res) => {
     } = req.params;
     vehicleModel.getVehicle(id, results => {
         if (results.length > 0) {
-            fs.rm(results[0].image, {}, function(err){
-                if(err) {
-                    return res.status(500).json({
-                        success: false,
-                        message: 'File not found'
-                    })
-                }
+            // fs.rm(results[0].image, {}, function(err){
+                // if(err) {
+                //     return res.status(500).json({
+                //         success: false,
+                //         message: 'File not found'
+                //     })
+                // }
+                const data = results[0]
                 return res.json({
                     success: true,
                     message: 'Detail Vehicle',
-                    results: results[0]
+                    results: data
                 });
-            })
+            // })
         } else {
             return res.status(404).json({
                 success: false,
@@ -82,8 +84,39 @@ const createVehicle = (req, res) => {
     })
 }
 
+const updateVehicle = async(req, res)=> {
+  const {id} = req.params
+  const result = await vehicleModel.getVehicleAsync(id)
+  if(result.length >= 1){
+    const data = {
+    }
+    // data["discount"] == data.discount
+    const fillable = ["price", "name", "qty", "image", "category_id"]
+    fillable.forEach(field => {
+      if(req.body[field]){
+        data[field] = req.body[field] // data.qty = req.body.qty
+      }
+    })
+    try{
+      const resultUpdate = await vehicleModel.updateVehicleAsync(data, id)
+      if(resultUpdate.affectedRows){
+        const fetchNew = await vehicleModel.getVehicleAsync(id)
+        return response(res, "Update Success", fetchNew[0])
+      }
+    }catch(err){
+      return response(res, "Unexpected Error", null, 400)
+    }
+  }else{
+    return res.status(400).json({
+      success: false,
+      message: 'Unexpected data'
+    })
+  }
+}
+
 module.exports = {
     getVehicles,
     getVehicle,
-    createVehicle
+    createVehicle,
+    updateVehicle
 };
